9e921eac39f594e46fcd5e6911755f6d657538b6
ViaceslaVV <160885897+ViaceslaVV@users.noreply.github.com>
2024-12-09 19:31:19 +0200

    New
    New

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 20f;
+    public int damage = 1;
+    public GameObject impactEffect;
+    public GameObject owner;
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = transform.right * speed;
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject == owner) return;
+
+        Instantiate(impactEffect, transform.position, transform.rotation);
+
+        if(other.gameObject.TryGetComponent(out Health health ))
+        {
+            health.Damage(damage);
+
+        }
+        Destroy(gameObject);
+    }
+}
+
